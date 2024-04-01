a5b91c674291edeb910d37003f3d0777b674bf8f
Milan Varughese <milanvarughese@outlook.com>
2024-04-01 15:29:43 +0000

    Initial commit. Vue, PWA and Bootstrap is working for now.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 createApp(App).mount('#app')
